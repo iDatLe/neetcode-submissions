f90494c87665f2f04cf7f9f9984d091c305ed0da
iDatLe <30909362+iDatLe@users.noreply.github.com>
2026-07-30 16:23:25 -0700

    Add: search-2d-matrix - submission-7

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-7.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        const rows = matrix.length;
+        const cols = matrix[0].length;
+
+        let left = 0;
+        let right = (rows * cols) -1;
+
+        while (left <= right) {
+            const middle = Math.floor((left + right) / 2);
+
+            const row = Math.floor(middle / cols);
+            const col = middle % cols;
+            const midValue = matrix[row][col]
+
+            if (target > midValue) {
+                left = middle + 1;
+            } else if (target < midValue) {
+                right = middle - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
