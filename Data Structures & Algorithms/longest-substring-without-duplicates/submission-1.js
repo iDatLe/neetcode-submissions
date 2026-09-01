28e6a533c28ac02b08fc832c729c1ff341503092
iDatLe <30909362+iDatLe@users.noreply.github.com>
2026-08-31 20:40:36 -0700

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let map = new Map();
+        let maxLength = 0;
+        let left = 0;
+
+        for (let right = 0; right < s.length; right++) {
+            if (map.has(s[right])) {
+                left = Math.max(map.get(s[right]) + 1, left);
+            }
+            map.set(s[right], right);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
