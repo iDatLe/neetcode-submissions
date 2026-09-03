bf5fb0712254a77e7a015a1ba9d6487642ba9fc9
iDatLe <30909362+iDatLe@users.noreply.github.com>
2026-09-02 22:06:57 -0700

    Add: design-hit-counter - submission-1

---
+++ Data Structures & Algorithms/design-hit-counter/submission-1.js
@@ -0,0 +1,31 @@
+class HitCounter {
+    constructor() {
+        this.hits = []
+    }
+
+    /**
+     * @param {number} timestamp
+     * @return {void}
+     */
+    hit(timestamp) {
+        this.hits.push(timestamp)
+    }
+
+    /**
+     * @param {number} timestamp
+     * @return {number}
+     */
+    getHits(timestamp) {
+        while (this.hits.length > 0 && timestamp - this.hits[0] >= 300) {
+            this.hits.shift();
+        }
+        return this.hits.length;
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * var obj = new HitCounter()
+ * obj.hit(timestamp)
+ * var param_2 = obj.getHits(timestamp)
+ */
